dfa3e7dc23840ef74cb8f27e1cb89f4e74f255fa
ianSama0217 <ian20000217@gmail.com>
2024-01-26 15:44:37 +0800

    spring init

---
+++ staffSeatsBackend/src/main/java/com/example/staffSeatsBackend/StaffSeatsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.staffSeatsBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffSeatsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffSeatsBackendApplication.class, args);
+	}
+
+}
---
+++ staffSeatsBackend/src/test/java/com/example/staffSeatsBackend/StaffSeatsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.staffSeatsBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffSeatsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
