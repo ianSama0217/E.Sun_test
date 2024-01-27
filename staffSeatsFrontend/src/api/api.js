12c161d9ae0f8f69aa4ab463382baf82586cbea6
ianSama0217 <ian20000217@gmail.com>
2024-01-27 20:17:35 +0800

    seatChart View debug

--- staffSeatsFrontend/src/api/api.js
+++ staffSeatsFrontend/src/api/api.js
@@ -15,4 +15,59 @@ const getAllSeat = () => {
     });
 };
 
-export default { getAllSeat };
+//顯示對應id座位
+const getSeatInfo = (id) => {
+  return axios
+    .get(`${API_URL}/seat/${id}`)
+    .then((res) => {
+      //console.log(res.data.seatInfo);
+      return res.data.seatInfo;
+    })
+    .catch((e) => {
+      throw e;
+    });
+};
+
+//顯示所有員工
+const getAllEmp = () => {
+  return axios
+    .get(`${API_URL}/employee`)
+    .then((res) => {
+      //console.log(res.data.employeeList);
+      return res.data.employeeList;
+    })
+    .catch((e) => {
+      throw e;
+    });
+};
+
+//選擇座位
+const insertUser = (req) => {
+  return axios({
+    url: `${API_URL}/seat/insert`,
+    method: "PATCH",
+    data: req,
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((e) => {
+      throw e;
+    });
+};
+
+//清除座位
+const clearUser = (seatId) => {
+  return axios({
+    url: `${API_URL}/seat/${seatId}`,
+    method: "PATCH",
+  })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((e) => {
+      throw e;
+    });
+};
+
+export default { getAllSeat, getSeatInfo, getAllEmp, insertUser, clearUser };
--- staffSeatsFrontend/src/views/SeatsView.vue
+++ staffSeatsFrontend/src/views/SeatsView.vue
@@ -1,13 +1,59 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, reactive } from "vue";
 import api from "../api/api.js";
 
-const { getAllSeat } = api;
+const { getAllSeat, getSeatInfo, getAllEmp, insertUser, clearUser } = api;
 
+//儲存所有座位資料
 const seats = ref([]);
+//儲存特定座位資料
+let seat = reactive({});
+//儲存員工資料
+const emps = ref([]);
+//select v-model
+const employeeId = ref(null);
+//控制彈跳視窗
+const isDisplay = ref(false);
+
+const isDisplayHandler = () => {
+  isDisplay.value = !isDisplay.value;
+};
+
+const clearEmp = () => {
+  employeeId.value = null;
+};
+
+//點擊確認按鈕，如果有employeeId就新增，沒有就刪除
+const setSeatHandler = async (seatId) => {
+  if (employeeId.value !== null) {
+    // insertUser請求資料
+    const req = {
+      emp_id: employeeId.value,
+      seat_id: seatId,
+    };
+    await insertUser(req);
+  } else {
+    await clearUser(seatId);
+  }
+
+  //重新印出更新後的資料
+  seats.value = await getAllSeat();
+  isDisplayHandler();
+};
+
+const getSeatInfoHandler = async (id) => {
+  seat = await getSeatInfo(id);
+  if (seat.employee !== null) {
+    employeeId.value = seat.employee.emp_id;
+  } else {
+    employeeId.value = null;
+  }
+  isDisplayHandler();
+};
 
 onMounted(async () => {
   seats.value = await getAllSeat();
+  emps.value = await getAllEmp();
 });
 </script>
 
@@ -16,6 +62,7 @@ onMounted(async () => {
     <div class="seatArea">
       <div v-for="(item, index) in seats" :key="index" class="seatChart">
         <div
+          @click="getSeatInfoHandler(item.seatingChart.floor_seat_seq)"
           class="seatBox"
           :class="{
             empty: item.seatingChart.state == '空位',
@@ -50,6 +97,33 @@ onMounted(async () => {
 
     <button type="button">送出</button>
   </div>
+
+  <!-- 彈跳視窗 -->
+  <div class="popWindow" v-if="isDisplay">
+    <p>{{ seat.seatingChart.floor_no }}樓</p>
+    <p>座位{{ seat.seatingChart.seat_no }}</p>
+    <span>選擇員工:</span>
+    <select v-model="employeeId">
+      <option v-if="seat.employee" :value="seat.employee.emp_id">
+        {{ seat.employee.emp_id }}
+      </option>
+      <option v-for="item in emps" :value="item.emp_id">
+        {{ item.emp_id }}
+      </option>
+    </select>
+
+    <button type="button" @click="clearEmp">清除員工資料</button>
+    <div class="btnbar">
+      <button type="button" @click="isDisplayHandler">取消</button>
+      <button
+        type="button"
+        @click="setSeatHandler(seat.seatingChart.floor_seat_seq)"
+      >
+        確認
+      </button>
+    </div>
+  </div>
+  <div class="background" v-if="isDisplay"></div>
 </template>
 
 <style scoped lang="scss">
@@ -137,5 +211,28 @@ onMounted(async () => {
       cursor: pointer;
     }
   }
+}
+
+.popWindow {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 30vw;
+  height: 50vh;
+  background-color: white;
+  border-radius: 8px;
+  z-index: 5;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+.background {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 1;
 }
 </style>
