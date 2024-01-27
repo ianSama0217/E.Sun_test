cd6441a77e4deec31ddfc0b21f15d0f9514bc3c3
ianSama0217 <ian20000217@gmail.com>
2024-01-28 00:09:00 +0800

    update DML

--- DB/DML.sql
+++ DB/DML.sql
@@ -1,35 +1,31 @@
 -- 新增員工資料
-INSERT INTO employee(EMP_ID, NAME, EMAIL)
-VALUES ("12345","IAN","123@GMAIL.COM")
-
--- 查詢員工資料
-SELECT EMP_ID, NAME, EMAIL  FROM employee
-WHERE EMP_ID = '12345';
-
--- 更新使用者(座位編號
-UPDATE employee SET FLOOR_SEAT_SEQ = '1F1'
-WHERE EMP_ID = '12345';
+INSERT INTO employee(EMP_ID, NAME, EMAIL, FLOOR_SEAT_SEQ) VALUES
+	( '77777', 'Mike', 'mike123@gmail.com', null),
+	( '24001', 'Jack', 'jack123@gmail.com', null),
+	( '24002', 'Tony', 'tony123@gmail.com', null),
+	( '24003', 'Amy', 'amy123@gmail.com', null),
+	( '24004', 'Lisa', 'lisa999@gmail.com', null),
+	( '24005', 'Elsa', 'elsa0622@gmail.com', null),
+	( '24006', 'John', 'john1312@gmail.com', null),
+	( '24007', 'Alex', 'alexzzz@gmail.com', null),
+	( '24008', 'Wick', 'wickkk@gmail.com', null),
+	( '24009', 'Tommy', 'tommyyyyy@gmail.com', null);
 	
 -- 新增座位資料
 INSERT INTO seatingchart(FLOOR_SEAT_SEQ, FLOOR_NO, SEAT_NO, STATE) VALUES
-	( '1F1', '1', '1', '空位');
-    
--- 照座位排序
-SELECT * FROM seatingchart
-WHERE FLOOR_NO = 2
-ORDER BY SEAT_NO DESC;
-
--- 合併座位及對應的員工id
-SELECT seat.FLOOR_SEAT_SEQ, FLOOR_NO, SEAT_NO, STATE, EMP_ID, NAME, EMAIL
-FROM seatingchart AS seat
-JOIN employee AS emp
-ON seat.FLOOR_SEAT_SEQ = emp.FLOOR_SEAT_SEQ
-WHERE seat.FLOOR_SEAT_SEQ = '1F1';
-
--- 更新座位狀態(新增使用者
-UPDATE seatingchart SET STATE = '已佔用'
-WHERE FLOOR_SEAT_SEQ = '1F1';
-
--- 更新座位狀態(移除使用者
-UPDATE seatingchart SET STATE = '空位'
-WHERE FLOOR_SEAT_SEQ = '1F1';
+	( '1F1', '1', '1', '空位'),
+	( '1F2', '1', '2', '空位'),
+	( '1F3', '1', '3', '空位'),
+	( '1F4', '1', '4', '空位'),
+	( '2F1', '2', '1', '空位'),
+	( '2F2', '2', '2', '空位'),
+	( '2F3', '2', '3', '空位'),
+	( '2F4', '2', '4', '空位'),
+	( '3F1', '3', '1', '空位'),
+	( '3F2', '3', '2', '空位'),
+	( '3F3', '3', '3', '空位'),
+	( '3F4', '3', '4', '空位'),
+	( '4F1', '4', '1', '空位'),
+	( '4F2', '4', '2', '空位'),
+	( '4F3', '4', '3', '空位'),
+	( '4F4', '4', '4', '空位');
