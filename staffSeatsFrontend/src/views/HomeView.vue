c760dc048d1010bb47da72ddae5252a2ba5aad19
ianSama0217 <ian20000217@gmail.com>
2024-01-27 20:40:57 +0800

    HomeView Style

--- staffSeatsFrontend/src/views/HomeView.vue
+++ staffSeatsFrontend/src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <script setup></script>
 
 <template>
-  <h1>首頁</h1>
+  <img src="../../public/homeImg.jpg" alt="首頁圖片" />
 </template>
+
+<style scoped lang="scss">
+img {
+  height: 100vh;
+  width: 80vw;
+}
+</style>
